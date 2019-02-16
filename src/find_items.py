5f21ac4e99d748b33bf366b1ecc891483a67b7fc
anderzzz <ohrn.anders@gmail.com>
2019-02-15 22:33:30 -0800

    Cloth colour guesser and beginnings of corner guesser

--- src/find_items.py
+++ src/find_items.py
@@ -2,7 +2,9 @@
 
 '''
 import sys
+
 import numpy as np
+from scipy import spatial 
 import matplotlib.pyplot as plt
 
 import skimage
@@ -16,6 +18,9 @@ DARK_RED   = (0.5, 0.0, 0.0)
 DARK_BLUE  = (0.0, 0.0, 0.5)
 BLACK      = (0.0, 0.0, 0.0)
 WHITE      = (1.0, 1.0, 1.0)
+
+class FileFormatError(Exception):
+    pass
 
 class Table:
 
@@ -46,13 +51,24 @@ class Table:
 
         return ret
 
+    def _cloth_luminance(self):
+
+        intensity = []
+        for rgb_point in self.img_obj.reshape(-1, 3):
+            intensity.append(max(0.0, 
+                1.0 - spatial.distance.euclidean(rgb_point, self.cloth_colour)))
+
+        return np.array(intensity).reshape(self.img_height, self.img_width, 3)
+
     def _guess_corners(self):
 
-        pass
+        fig_cloth_lum = self._cloth_luminance()
 
-    def find_corners(self):
+        return c_00, c_01, c_10, c_11
 
-        self.corners = self._guess_corners()     
+    def _optimize_corners(self):
+
+        p_00, p_01, p_10, p_11 = self._guess_corners()     
 
     def find_table(self):
 
@@ -82,18 +98,40 @@ class Table:
 
     def __init__(self, img_path):
 
+        #
+        # Path to file
         self.img_path = img_path
-        raw_image = io.imread(self.img_path)
-        rgb_image = color.rgba2rgb(raw_image)
-        float_image = util.img_as_float(rgb_image)
-        self.img_obj = float_image 
 
+        #
+        # Read the image data and put into proper object
+        raw_image = io.imread(self.img_path)
+        if len(raw_image.shape) == 3:
+            if raw_image.shape[2] == 4:
+                rgb_image = color.rgba2rgb(raw_image)
+
+            elif raw_image.shape[2] == 3:
+                rgb_image = raw_image
+
+            else:
+                raise FileFormatError('Image {0} yields '.format(self.img_path) + \
+                                      'image data of wrong shape')
+
+        else:
+            raise FileFormatError('Image {0} yields '.format(self.img_path) + \
+                                  'image data of wrong shape')
+        self.img_obj = util.img_as_float(rgb_image)
+        self.img_height = self.img_obj.shape[0]
+        self.img_width = self.img_obj.shape[1]
+
+        #
+        # Guess the cloth colour
         self.cloth_color = self._guess_cloth_colour()
 
+        #
+        # Guess the corner coordinates
         self.n_corners = 4
-        self.corners = [None] * self.n_corners
+        self.corners = self._optimize_corners()
 
-        raise RuntimeError
 
 if __name__ == '__main__':
 
